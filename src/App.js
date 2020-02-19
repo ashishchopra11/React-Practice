95f7662e3a0c863fe143bae586c1101daf7ce7b9
Ashish Chopra <ashish1194.cse16@chitkara.edu.in>
2020-02-19 21:18:02 +0530

    State and props done and set State and bind in state implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:'max',age:28},
+      {name:'manu',age:29},
+      {name:'Stephanie', age:26}
+    ]
+  }
+  switchNameHandler = (newName) =>{
+    // console.log('was clicked');
+    this.setState({
+      persons: [
+        {name:'Ashish',age:23},
+        {name:'rawat',age:22},
+        {name:'Stephanie', age:26}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Hi , I am Sanjay.....Sanjay Singhania</h1>
+      <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+      <Person 
+      name={this.state.persons[0].name}   
+      age={this.state.persons[0].age}/>
+      <Person
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age}/>
+      <Person
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age}
+         click = {this.switchNameHandler.bind(this, 'MAX!!!!!!!')}>My Hobbies : Racing</Person>
+      
       </div>
     );
   }
