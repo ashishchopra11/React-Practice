ff847c0f81a04b9a684f11388f481a51ab9f8233
Ashish Chopra <ashish1194.cse16@chitkara.edu.in>
2020-03-11 14:08:05 +0530

    Code implemented using components

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -3,12 +3,7 @@ import classes from './Person.css';
 // import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 const person = (props) =>{
-    // const rnd = Math.random();
-    // if(rnd > 0.9){
-    //      throw new Error('Something went wrong' );
-    // }
     return (
-//    <div className="Person" style={style}> 
        <div className={classes.Person}>
             <p onClick={props.click}> I'm {props.name} and  i am {props.age} years old!'</p>
             <p>{props.children}</p>
